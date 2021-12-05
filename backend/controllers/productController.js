a43dbb5dddbdadbba12512a68199df14829dd73d
Syket Das <saiketsd23@gmail.com>
2021-12-05 14:50:33 +0530

    create new product route created

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,8 +1,19 @@
+const Product = require('../models/product');
 
+// create new product  => /api/v1/product/new
 
-exports.getProducts = (req,res, next) =>{
-    res.status(200).json({
-        success: true,
-        message: 'This route will show all products from database',
-    });
-}
+exports.newProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+  res.status(201).json({
+    success: true,
+
+    product,
+  });
+};
+
+exports.getProducts = (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    message: 'This route will show all products from database',
+  });
+};
