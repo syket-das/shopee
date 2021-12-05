d935148c9dbf6a5dd94c00b45fee607be442ed0d
Syket Das <saiketsd23@gmail.com>
2021-12-05 17:02:23 +0530

    single product id bug fix

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,5 @@
+
+const mongoose = require('mongoose');
 const Product = require('../models/product');
 
 // create new product  => /api/v1/product/new
@@ -11,8 +13,8 @@ exports.newProduct = async (req, res, next) => {
   });
 };
 
-exports.getProducts = async(req, res, next) => {
-  const products = await Product.find()
+exports.getProducts = async (req, res, next) => {
+  const products = await Product.find();
   res.status(200).json({
     success: true,
     products,
@@ -21,10 +23,25 @@ exports.getProducts = async(req, res, next) => {
 
 // get single product => /api/v1/product/:id
 
-exports.getSingleProduct = async(req, res, next) => {
+exports.getSingleProduct = async (req, res, next) => {
+
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(400).json({
+      success: false,
+      message: 'Invalid ID',
+    });
+  }
+
+
   const product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: 'Product not found',
+    });
+  }
   res.status(200).json({
     success: true,
     product,
   });
-}
+};
