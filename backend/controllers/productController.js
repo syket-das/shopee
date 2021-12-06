44420cdb638c4d505aef3755e76679077474afce
Syket Das <saiketsd23@gmail.com>
2021-12-06 15:43:29 +0530

    s

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
-
 const mongoose = require('mongoose');
 const Product = require('../models/product');
+
+const ErrorHandler = require('../utils/errorHandler')
 
 // create new product  => /api/v1/product/new
 
@@ -17,6 +18,7 @@ exports.getProducts = async (req, res, next) => {
   const products = await Product.find();
   res.status(200).json({
     success: true,
+    count: products.length,
     products,
   });
 };
@@ -24,7 +26,24 @@ exports.getProducts = async (req, res, next) => {
 // get single product => /api/v1/product/:id
 
 exports.getSingleProduct = async (req, res, next) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return next(new ErrorHandler('Invalid ID', 400));
+  }
 
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new ErrorHandler('Product not found', 404));
+  }
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+// update product  => /api/v1/product/:id
+
+exports.updateProduct = async (req, res, next) => {
+  console.log(req.body);
   if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
     return res.status(400).json({
       success: false,
@@ -32,16 +51,48 @@ exports.getSingleProduct = async (req, res, next) => {
     });
   }
 
+  let product = await Product.findById(req.params.id);
 
-  const product = await Product.findById(req.params.id);
   if (!product) {
     return res.status(404).json({
       success: false,
       message: 'Product not found',
     });
   }
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
   res.status(200).json({
     success: true,
     product,
   });
 };
+
+
+// delete product  => /api/v1/product/:id
+
+ exports.deleteProduct = async (req, res, next) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(400).json({
+      success: false,
+      message: 'Invalid ID',
+    });
+  }
+
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: 'Product not found',
+    });
+  }
+  await Product.findByIdAndDelete(req.params.id);
+  res.status(200).json({
+    success: true,
+    message: 'Product deleted successfully',
+  });
+}
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -5,12 +5,18 @@ const {
   getProducts,
   newProduct,
   getSingleProduct,
+  updateProduct,
+  deleteProduct,
 } = require('../controllers/productController');
 
 router.route('/products').get(getProducts);
 
-router.route('/product/new').post(newProduct);
-
 router.route('/product/:id').get(getSingleProduct);
+
+router.route('/admin/product/new').post(newProduct);
+
+router.route('/admin/product/:id').put(updateProduct);
+
+router.route('/admin/product/:id').delete(deleteProduct);
 
 module.exports = router;
