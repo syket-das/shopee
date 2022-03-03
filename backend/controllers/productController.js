1560d83000728174373ab327546a89a163f339aa
Syket Das <80612737+syket-das@users.noreply.github.com>
2022-03-03 13:49:13 +0530

    pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -17,15 +17,20 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 // get all products => /api/v1/products
 
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
+  const resPerPage = 5;
+  const productCount = await Product.countDocuments();
+
   const apiFeatures = new APIFeatures(Product.find(), req.query)
     .search()
-    .filter();
+    .filter()
+    .pagination(resPerPage);
 
   // const products = await Product.find();
   const products = await apiFeatures.query;
   res.status(200).json({
     success: true,
     count: products.length,
+    productCount,
     products,
   });
 });
