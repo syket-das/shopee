83c33c351abf19cc30703c47b37076b6d60e1b4e
Syket Das <80612737+syket-das@users.noreply.github.com>
2022-03-08 14:08:31 +0530

    order routes finished

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -35,3 +35,98 @@ exports.newOrder = catchAsyncErrors(async (req, res, next) => {
     order,
   });
 });
+
+// Get Single Order => /api/v1/order/:id
+
+exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  );
+
+  if (!order) {
+    return next(new ErrorHandler('No order found!', 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+// Get Logged in user Orders => /api/v1/orders/me
+
+exports.myOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find({ user: req.user.id });
+
+  res.status(200).json({
+    success: true,
+    orders,
+  });
+});
+
+// Get all Orders => /api/v1/admin/orders/
+
+exports.allOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find();
+
+  let totalAmount = 0;
+
+  orders.forEach((order) => {
+    totalAmount += order.totalPrice;
+  });
+
+  res.status(200).json({
+    success: true,
+    totalAmount,
+    orders,
+  });
+});
+
+// Update Order => /api/v1/admin/order/:id
+
+exports.updateOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order.orderStatus === 'Delivered') {
+    return next(new ErrorHandler('Order already delivered', 400));
+  }
+
+  order.orderItems.forEach(async (item) => {
+    await updateStock(item.product, item.quantity);
+  });
+
+  order.orderStatus = req.body.status;
+  order.deliveredAt = Date.now();
+
+  await order.save();
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+async function updateStock(id, quantity) {
+  const product = await Product.findById(id);
+
+  product.stock = product.stock - quantity;
+
+  await product.save({ validateBeforeSave: false });
+}
+
+// Delete Order => /api/v1/admin/order/:id
+
+exports.deleteOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    return next(new ErrorHandler('No order found!', 404));
+  }
+
+  await order.remove();
+
+  res.status(200).json({
+    success: true,
+  });
+});
